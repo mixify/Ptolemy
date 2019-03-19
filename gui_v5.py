fc773255cb2b111034f5fb7f7a61afc3de1bb5a1
cpark7 <47586415+cpark7@users.noreply.github.com>
2019-03-19 21:51:03 +0900

    still traing for gui
    so happy

--- gui_v5.py
+++ gui_v5.py
@@ -5,11 +5,12 @@
 
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QToolTip
-from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QAction, qApp
+from PyQt5.QtGui import QFont,QIcon
+from tkinter import *
+import tkinter.messagebox
 
-
-class MyApp(QMainWindow):
+class MyApp:
 
     def __init__(self):
         super().__init__()
@@ -17,28 +18,50 @@ class MyApp(QMainWindow):
         self.init_ui()
 
     def init_ui(self):
-
-        QToolTip.setFont(QFont('SansSerif', 10))
-#        self.setToolTip('This is a <b>QWidget</b> widget')
-
-        self.statusBar().showMessage('연습 중..')
-
-        btn = QPushButton('버튼1', self)
-        btn2 = QPushButton('버튼2', self)
-        btn3 = QPushButton('버튼3', self)
-        btn.move(50, 100)
-        btn2.move(150, 100)
-        btn3.move(250, 100)
         
-        btn.resize(btn.sizeHint())
-        btn2.resize(btn.sizeHint())
-        btn3.resize(btn.sizeHint())
+#        menu_bar = self.menuBar()
+        
+        
+        exitAction = QAction(QIcon('exit.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
 
+ #       self.statusBar()
 
+        btn1 = QPushButton('녹화',self)
+        btn2 = QPushButton('시작',self)
+        btn3 = QPushButton('중지',self)
+        
+        hbox = QHBoxLayout()
+        hbox.addStretch(10)
+        hbox.addWidget(btn1)
+        hbox.addStretch(10)
+        hbox.addWidget(btn2)
+        hbox.addStretch(10)
+        hbox.addWidget(btn3)
+        hbox.addStretch(10)
 
+        vbox = QVBoxLayout()
+        vbox.addStretch(9)
+        vbox.addLayout(hbox)
+        vbox.addStretch(1)
+
+        btn1.clicked.connect(self.print_btn1)
+#        btn2.clicked.connect(self.print_btn2)
+ #       btn3.clicked.connect(self.print_btn3)
+
+        self.setLayout(vbox)
         self.setWindowTitle('Ptolemy')
-        self.setGeometry(500, 500, 500, 200)
+        self.setGeometry(500, 500, 500, 500)
         self.show()
+
+    def print_btn1(self):
+        tkinter.messagebox.showinfo("tlqkf","tlqkf")
+ #   def print_btn2(self):
+        
+#    def print_btn3(self):
+        
 
 if __name__ == '__main__':
 
