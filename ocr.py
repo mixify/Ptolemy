a915e0228e430553dccf645fc3b17bff4238c623
Oh Seong <ohsg0315@naver.com>
2019-03-26 22:00:48 +0900

    Image matching

--- ocr.py
+++ ocr.py
@@ -3,6 +3,6 @@ from PIL import Image
 
 img = Image.open('test.png')
 
-#pytesseract.pytesseract.tesseract_cmd =
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
 result = pytesseract.image_to_string(img)
 print(result)
