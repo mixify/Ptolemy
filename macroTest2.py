4dd922b4c19ef007ab3f9263db8347855eac8fec
EGKANG <dmsrn3@naver.com>
2019-03-28 22:01:44 +0900

    keylogger 클래스화, macro test 코드 추가

--- macroTest2.py
+++ macroTest2.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QTimer
 import win32api
 import pyautogui
+import pyHook
 import pyHookKeyLogger as KL
 
 class MyApp(QWidget):
@@ -10,52 +11,47 @@ class MyApp(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.x = QLineEdit()
-        self.t_le = QLineEdit()
-        self.start_btn = QPushButton('Start', self)
-        self.timer = QTimer()
+        self.keylogger = KL.KeyLogger()
+        self.hm = pyHook.HookManager()
+        self.hm.KeyDown = self.keylogger.OnKeyDownEvent
+        self.hm.KeyUp = self.keylogger.OnKeyUpEvent
 
+        self.start_btn = QPushButton('Start', self)
+        self.stop_btn = QPushButton('Stop', self)
         self.initUI()
 
     def initUI(self):
 
         hbox1 = QHBoxLayout()
         hbox1.addStretch(1)
-        hbox1.addWidget(QLabel('String: '))
-        hbox1.addWidget(self.x)
+        hbox1.addWidget(self.start_btn)
         hbox1.addStretch(1)
 
         hbox2 = QHBoxLayout()
         hbox2.addStretch(1)
-        hbox2.addWidget(QLabel('Num of Iteration: '))
-        hbox2.addWidget(self.t_le)
+        hbox2.addWidget(self.stop_btn)
         hbox2.addStretch(1)
-
-        hbox3 = QHBoxLayout()
-        hbox3.addStretch(1)
-        hbox3.addWidget(self.start_btn)
-        hbox3.addStretch(1)
 
         vbox = QVBoxLayout()
         vbox.addLayout(hbox1)
         vbox.addLayout(hbox2)
-        vbox.addLayout(hbox3)
 
         self.setLayout(vbox)
 
         self.start_btn.clicked.connect(self.start_btn_click)
+        self.stop_btn.clicked.connect(self.stop_btn_click)
 
         self.setWindowTitle('Ptolemy')
         self.setGeometry(300, 300, 300, 200)
         self.show()
 
     def start_btn_click(self):
+        self.hm.HookKeyboard()
 
-        num = int(self.t_le.text())
-        KL.hm.HookKeyboard()
-        for i in range(num):
-            pyautogui.typewrite(self.x.text(), interval=0.5)
-        KL.saveKeyLog()
+    def stop_btn_click(self):
+
+        self.keylogger.saveKeyLog()
+        self.hm.UnhookKeyboard()
 
 
 if __name__ == '__main__':
--- pyHookKeyLogger.py
+++ pyHookKeyLogger.py
@@ -1,41 +1,60 @@
 
 import pythoncom, pyHook
 
-def OnKeyboardEvent(event):
-    buffer = ''
-    print ('MessageName:',event.MessageName)
-    print ('Message:',event.Message)
-    print ('Time:',event.Time)
-    print ('Window:',event.Window)
-    print ('WindowName:',event.WindowName)
-    print ('Ascii:', event.Ascii, chr(event.Ascii))
-    print ('Key:', event.Key)
-    print ('KeyID:', event.KeyID)
-    print ('ScanCode:', event.ScanCode)
-    print ('Extended:', event.Extended)
-    print ('Injected:', event.Injected)
-    print ('Alt', event.Alt)
-    print ('Transition', event.Transition)
-    print ('-----------------------')
+class KeyLogger:
 
-    if event.Ascii != 0 or 8: # extended, backspace 제외
-        f = open('keylogger.txt', mode = 'at')
-        keylogs = chr(event.Ascii) 
-        if event.Ascii == 13: # enter 입력
-            keylogs = '\n'
-        buffer += keylogs 
-        f.write(buffer)
+    def __init__(self):
+        self.saveKey = []
+        self.tmp_time = -1
+
+    def OnKeyDownEvent(self, event):
+        #global tmp_time
+        if self.tmp_time == -1:
+            self.tmp_time = event.Time
+        
+        # return True to pass the event to other handlers
+        return True
+
+    def OnKeyUpEvent(self, event):
+        #global tmp_time
+        print ('MessageName:',event.MessageName)
+        print ('Message:',event.Message)
+        print ('Time:',event.Time)
+        print ('Window:',event.Window)
+        print ('WindowName:',event.WindowName)
+        print ('Ascii:', event.Ascii, chr(event.Ascii))
+        print ('Key:', event.Key)
+        print ('KeyID:', event.KeyID)
+        print ('ScanCode:', event.ScanCode)
+        print ('Extended:', event.Extended)
+        print ('Injected:', event.Injected)
+        print ('Alt', event.Alt)
+        print ('Transition', event.Transition)
+        print ('-----------------------')
+
+        self.saveKey.append(event.Key)
+        self.saveKey.append(' ')
+        self.saveKey.append(float(event.Time - self.tmp_time)/1000)
+        self.saveKey.append('\n')
+        self.tmp_time = -1
+
+        # return True to pass the event to other handlers
+        return True
+
+    def saveKeyLog(self):
+        f = open("keylog.txt",'wt', encoding='UTF-8')
+        test="".join(str(s) for s in self.saveKey)
+        f.write(test)
         f.close()
 
-# return True to pass the event to other handlers
-    return True
-
 if __name__ == '__main__':
+
+    keylogger = KeyLogger()
     # create a hook manager
     hm = pyHook.HookManager()
     # watch for all keyboard events
-    hm.KeyDown = OnKeyboardEvent
-    # hm.KeyUp = OnKeyboardEvent
+    hm.KeyDown = keylogger.OnKeyDownEvent
+    hm.KeyUp = keylogger.OnKeyUpEvent
     # set the hook
     hm.HookKeyboard()
     # wait forever
