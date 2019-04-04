2a88a9f592f24f586fc34e2c433cab4e62073615
cpark7 <47586415+cpark7@users.noreply.github.com>
2019-04-04 21:17:15 +0900

    GUI folder create
    make just main with add

---
+++ gui/main_ui.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main.ui'
+#
+# Created by: PyQt5 UI code generator 5.12
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(725, 639)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 10, 721, 541))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.pushButton_2 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_2.setCheckable(True)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.verticalLayout.addWidget(self.pushButton_2)
+        self.pushButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        self.pushButton_3 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.verticalLayout.addWidget(self.pushButton_3)
+        self.pushButton_4 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.verticalLayout.addWidget(self.pushButton_4)
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 1, 1, 1)
+        self.listView = QtWidgets.QListView(self.gridLayoutWidget)
+        self.listView.setObjectName("listView")
+        self.gridLayout.addWidget(self.listView, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 725, 21))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menu = QtWidgets.QMenu(self.menuFile)
+        self.menu.setObjectName("menu")
+        self.menuHelp = QtWidgets.QMenu(self.menubar)
+        self.menuHelp.setObjectName("menuHelp")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.action_2 = QtWidgets.QAction(MainWindow)
+        self.action_2.setObjectName("action_2")
+        self.action_4 = QtWidgets.QAction(MainWindow)
+        self.action_4.setObjectName("action_4")
+        self.action_6 = QtWidgets.QAction(MainWindow)
+        self.action_6.setObjectName("action_6")
+        self.action_7 = QtWidgets.QAction(MainWindow)
+        self.action_7.setObjectName("action_7")
+        self.action_8 = QtWidgets.QAction(MainWindow)
+        self.action_8.setObjectName("action_8")
+        self.menu.addAction(self.action_7)
+        self.menu.addAction(self.action_8)
+        self.menuFile.addAction(self.menu.menuAction())
+        self.menuFile.addAction(self.action_2)
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.action_4)
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.action_6)
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuHelp.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton_2.setText(_translate("MainWindow", "추가"))
+        self.pushButton.setText(_translate("MainWindow", "실행"))
+        self.pushButton_3.setText(_translate("MainWindow", "수정"))
+        self.pushButton_4.setText(_translate("MainWindow", "삭제"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.menu.setTitle(_translate("MainWindow", "새 매크로 만들기"))
+        self.menuHelp.setTitle(_translate("MainWindow", "Help"))
+        self.action_2.setText(_translate("MainWindow", "매크로 불러오기"))
+        self.action_4.setText(_translate("MainWindow", "단축키 설정"))
+        self.action_6.setText(_translate("MainWindow", "종료"))
+        self.action_7.setText(_translate("MainWindow", "일반 매크로 생성"))
+        self.action_8.setText(_translate("MainWindow", "딥러닝 매크로 생성"))
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
