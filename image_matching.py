860248b7e21cea7c9fa50ee617227ac0afe51499
Oh Seong <ohsg0315@naver.com>
2019-04-16 22:42:45 +0900

    multiple matching

--- image_matching.py
+++ image_matching.py
@@ -1,30 +1,21 @@
-import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
-img = cv.imread('origin.jpg',0)
-img2 = img.copy()
-template = cv.imread('cmp2.png',0)
-w, h = template.shape[::-1]
-# All the 6 methods for comparison in a list
-methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
-            'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
-for meth in methods:
-    img = img2.copy()
-    method = eval(meth)
-    # Apply template Matching
-    res = cv.matchTemplate(img,template,method)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv.rectangle(img,top_left, bottom_right, 255, 2)
-    plt.subplot(121),plt.imshow(res,cmap = 'gray')
-    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(img,cmap = 'gray')
-    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    plt.suptitle(meth)
-    plt.show()
+import cv2
 
+def tmpMatching(thr):
+    img = cv2.imread('nuguri.jpg')
+    imgray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    template = cv2.imread('bam.jpg', cv2.IMREAD_GRAYSCALE)
+    w, h = template.shape[::-1]
+
+    res = cv2.matchTemplate(imgray, template, cv2.TM_CCOEFF_NORMED)
+
+    loc = np.where(res >= thr)
+
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+
+        cv2.imshow('res', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+tmpMatching(0.92)
