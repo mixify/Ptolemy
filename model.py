94f53a0a4aea7e04220837531250f12efe71c032
mixify <mixify@naver.com>
2019-05-21 22:04:00 +0900

    for testing

--- model.py
+++ model.py
@@ -39,7 +39,7 @@ class CnnDQN(nn.Module):
         self.num_actions = num_actions
 
         self.features = nn.Sequential(
-            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=3),
             nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=4, stride=2),
             nn.ReLU(),
